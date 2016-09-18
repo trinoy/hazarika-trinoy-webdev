3e12d73e0821ac5cce8b3ae1eaee491789a0b0e2
Trinoy <trinoy@Trinoys-MacBook-Pro.local>
2016-09-18 18:30:42 -0400

    fixed mongo db path error1

--- test/app.js
+++ test/app.js
@@ -8,7 +8,7 @@ module.exports = function(app)
     //var connectionString = 'mongodb://webdev1:ABcd1234@ds033126.mlab.com:33126/mongo_th';
 
     if(process.env.HEROKU_MONGODB_DB_PASSWORD) {
-        connectionString = HEROKU_MONGODB_URL_PREFIX +
+        connectionString = process.env.HEROKU_MONGODB_URL_PREFIX +
             process.env.HEROKU_MONGODB_DB_USERNAME + ":" +
             process.env.HEROKU_MONGODB_DB_PASSWORD +
             process.env.HEROKU_MONGODB_URL_SUFFIX +
